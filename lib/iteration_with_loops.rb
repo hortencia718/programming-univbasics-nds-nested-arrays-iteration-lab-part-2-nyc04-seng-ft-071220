8088fa0d61b5547d88250834ed88db4b9290c49f
hortencia c <hortenciaecisneros@gmail.com>
2020-07-24 12:55:05 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,5 +1,21 @@
 def find_min_in_nested_arrays(src)
+  new_array =[]
+  
+  while src < new_array.length do
+    
+  
+   
+  
+   
+  end
   # src will be an array of arrays of integers
   # Produce a new Array that contains the smallest number of each of the nested arrays
 
 end
+# do that takes in an "one argument" of an AOArrays that contains sets of numbers"
+
+# it "returns the smallest numbers from each set in a new Array" do
+      expect(find_
+
+
+# find_min_in_nested_arrays([19, 21, 24, 26, 30, 34, 37, 39, 40, 45, 48, 50, 55, 60, 63, 59, 49, 45, 40, 39, 34, 32, 25, 18])
